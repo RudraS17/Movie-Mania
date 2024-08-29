773e1ce2399a17c894c3af70e7979ca7cdc78eb0
RudranshShrivastav <rudranshsr@gmail.com>
2024-08-29 22:11:58 +0530

    second-commit

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function Home(){
-    return(<h1>Home</h1>)
+    return(<div>
+        
+        <h1>Super App</h1>
+        <h2>Create an account</h2>
+
+        <input type="text" placeholder="Name"/>
+        <input type="text" placeholder="UserName"/>
+        <input type="text" placeholder="email" />
+        <input type="text" placeholder="Mobile" />
+        <div>
+        <input type="checkbox" id="checkbox"/> 
+        <label htmlFor="checkbox" placeholder="Share my registration data with Superapp" ></label>
+        </div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+    )
 }
 
 export default Home;
